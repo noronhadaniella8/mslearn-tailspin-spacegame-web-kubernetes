34bd38b6945f6ab05b39a5798164dba7f46edf4e
Daniella Noronha <dnoronha@microsoft.com>
2021-08-11 17:29:59 -0400

    Possible bugfix

--- Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
@@ -21,7 +21,9 @@ namespace TailSpin.SpaceGame.Web
             using (WebClient webClient = new WebClient())
             {
                 string json = await webClient.DownloadStringTaskAsync($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}");
-                return JsonSerializer.Deserialize<LeaderboardResponse>(json);
+                var leaderboardResponse = JsonSerializer.Deserialize<LeaderboardResponse>(json, new JsonSerializerOptions { IncludeFields = true });
+                return leaderboardResponse;
+                //return JsonSerializer.Deserialize<LeaderboardResponse>(json);
             }
         }
     }
--- Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardResponse.cs
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardResponse.cs
@@ -5,18 +5,25 @@ namespace TailSpin.SpaceGame.Web
 {
     public class LeaderboardResponse
     {
-        // The game mode selected in the view.
+         // The game mode selected in the view.
+        [JsonPropertyName("selectedMode")]
         public string SelectedMode { get; set; }
+
         // The game region (map) selected in the view.
+        [JsonPropertyName("selectedRegion")]
         public string SelectedRegion { get; set; }
         // The current page to be shown in the view.
+        [JsonPropertyName("page")]
         public int Page { get; set; }
         // The number of items to show per page in the view.
+        [JsonPropertyName("pageSize")]
         public int PageSize { get; set; }
 
         // The scores to display in the view.
+        [JsonPropertyName("scores")]
         public IEnumerable<ScoreProfile> Scores { get; set; }
         // The total number of results for the selected game mode and region in the view.
+        [JsonPropertyName("totalResults")]
         public int TotalResults { get; set; }
     }
 }
